b8ae02efd4ab8277ff45d80e309768c88fcbc8d4
amportugal <nunommarquespt@gmail.com>
2018-04-09 21:16:37 +0100

    Refined unit test to only test the splitting.

--- splitter-truffle/contracts/Splitter.sol
+++ splitter-truffle/contracts/Splitter.sol
@@ -2,46 +2,29 @@ pragma solidity ^0.4.18;
 
 contract Splitter {
     address     public owner;
-    uint        public balance;
-    
+
     // Address of people
-    address     public aliceAddress;
     address     public bobAddress;
     address     public carolAddress;
 
     // Event logger
-    event LogContractPay(address sender, uint amount);
+    event LogContractPay(address recipient1, address recipient2, uint amountRecipient1, uint amountRecipient2);
 
     function Splitter(address aliceAddr, address bobAddr, address carolAddr) public {
-        owner = msg.sender;
+        owner = aliceAddr;
 
-        aliceAddress = aliceAddr;
         bobAddress = bobAddr;
         carolAddress = carolAddr;
-
-        // Balance is already 0 by default
     }
 
-    /*function getBalance() public constant returns (uint balance) {
-        return balance;
-    }*/
-
-    //Fallback function
     function() public payable{
         require(msg.value > 0);
-        require(isContractAllowedAddrs(msg.sender));
+        require(owner == msg.sender);
 
-        if(msg.sender == aliceAddress) {
-            uint halfOfValue = msg.value / 2;
-            if(!bobAddress.send(halfOfValue)) revert();
-            if(!carolAddress.send(halfOfValue)) revert();
-        } else {
-            balance += msg.value;
-            LogContractPay(msg.sender, msg.value);
-        }
-    }
+        uint halfOfValue = msg.value / 2;
+        if(!bobAddress.send(halfOfValue)) revert();
+        if(!carolAddress.send(halfOfValue)) revert();
 
-    function isContractAllowedAddrs(address addr) private constant returns (bool) {
-        return  (addr == aliceAddress || addr == bobAddress || addr == carolAddress);
+        LogContractPay(bobAddress, carolAddress, halfOfValue, halfOfValue);
     }
 }
--- splitter-truffle/test/Splitter.js
+++ splitter-truffle/test/Splitter.js
@@ -3,73 +3,31 @@ var Splitter = artifacts.require("./Splitter.sol");
 contract("Splitter contract", function(accounts) {
 
     var owner = accounts[0];
-    var aliceAddress = accounts[1];
-    var bobAddress = accounts[2];
-    var carolAddress = accounts[3];
+    var bobAddress = accounts[1];
+    var carolAddress = accounts[2];
 
-    var aliceWeiPay = 2000;
-    var bobWeiPay = 1000;
-    var carolWeiPay = 5000;
+    var aliceWeiPay = 5000;
 
     var contract;
 
     beforeEach(function() {
-        return Splitter.new(aliceAddress, bobAddress, carolAddress, {from: owner})
+        return Splitter.new(owner, bobAddress, carolAddress, {from: owner})
                 .then( instance => contract = instance);
     })
 
-    it("should have starting balance equal to 0", function() {
-        return contract.balance({from: owner})
-                .then(_balance => assert.strictEqual(_balance.toString(10), '0', "Balance is not 0"));
-    });
-
-    it("should have correct addresses", function() {
-        return contract.aliceAddress({from: owner})
-                .then(function(_aliceAddress) {
-                    assert.strictEqual(_aliceAddress, aliceAddress, "Incorrect Alice's address");
-                    return contract.bobAddress({from: owner});
-                })
-                .then(function(_bobAddress) {
-                    assert.strictEqual(_bobAddress, bobAddress, "Incorrect Bob's address");
-                    return contract.carolAddress({from: owner});
-                })
-                .then(function(_carolAddress) {
-                    assert.strictEqual(_carolAddress, carolAddress, "Incorrect Carol's address");
-                })
-    })
-
-    it("should process payments to the contract", function() {
-        var bobBalance;
-        var carolBalance;
-        
+    it("should split payment of alice to bob and carol", function() {
+        var bobBalance = web3.eth.getBalance(bobAddress);
+        var carolBalance = web3.eth.getBalance(carolAddress);
         var bobNewBalance;
         var carolNewBalance;
 
-        var contractBalance;
-
-        return contract.sendTransaction({from: bobAddress, value: bobWeiPay})
+        return contract.sendTransaction({from: owner, value: aliceWeiPay})
             .then(function(tx) {
-                contractBalance = web3.eth.getBalance(contract.address).toString(10);
-
-                assert.equal(contractBalance, bobWeiPay.toString(10), "Contract's balance is wrong.");
-                return contract.sendTransaction({from: carolAddress, value: carolWeiPay})
-            })
-            .then(function(tx) {
-                contractBalance = web3.eth.getBalance(contract.address).toString(10);
-
-                bobBalance = web3.eth.getBalance(bobAddress).toString(10);
-                carolBalance = web3.eth.getBalance(carolAddress).toString(10);
-
-                assert.equal(contractBalance, (bobWeiPay + carolWeiPay).toString(10), "Contract's balance is wrong.");
-                return contract.sendTransaction({from: aliceAddress, value: aliceWeiPay})
-            })
-            .then(function(tx) {
-                bobNewBalance = web3.eth.getBalance(bobAddress).toString(10);
-                carolNewBalance = web3.eth.getBalance(carolAddress).toString(10);
-
-                assert.equal(contractBalance, (bobWeiPay + carolWeiPay).toString(10), "Contract's balance mustn't be different.");
-                assert.equal(bobNewBalance, (bobBalance + (aliceWeiPay/2)).toString(10), "Bob's balance is wrong.");
-                assert.equal(carolNewBalance, (carolBalance + (aliceWeiPay/2)).toString(10), "Carol's balance is wrong.");
+                bobNewBalance = web3.eth.getBalance(bobAddress);
+                carolNewBalance = web3.eth.getBalance(carolAddress);
+                
+                assert.equal(bobNewBalance.toString(10), bobBalance.plus(aliceWeiPay/2).toString(10), "Bob's balance is wrong.");
+                assert.equal(carolNewBalance.toString(10), carolBalance.plus(aliceWeiPay/2).toString(10), "Carol's balance is wrong.");
             });
     })
 
